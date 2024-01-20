89a96f1e7f87150a2fa7134d4aba35054110788c
sangwon432 <swonl0622@gmail.com>
2024-01-20 06:07:43 -0600

    Added Signup, encryption, and profile image generartion

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UserService } from '../user/user.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(private readonly userService: UserService) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async createUser(createUserDto: CreateUserDto) {
+    const newUser = await this.userService.createUser(createUserDto);
+    newUser.password = undefined;
+    return newUser;
   }
 }
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,8 +1,11 @@
-import { BaseEntity, Column } from 'typeorm';
+import { BaseEntity } from '../../common/base.entity';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+import * as gravatar from 'gravatar';
 
+@Entity()
 export class User extends BaseEntity {
-  //username, email, password,
-
+  // username, email, password,
   @Column()
   public username: string;
 
@@ -11,4 +14,22 @@ export class User extends BaseEntity {
 
   @Column()
   public password: string;
+
+  @Column({ nullable: true })
+  public profileImg?: string;
+
+  @BeforeInsert()
+  async beforeSaveFunction(): Promise<void> {
+    // profile image 자동 생성
+    this.profileImg = gravatar.url(this.email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm',
+      protocol: 'https',
+    });
+
+    //password 암호화
+    const saltValue = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, saltValue);
+  }
 }

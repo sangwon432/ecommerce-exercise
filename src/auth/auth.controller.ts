ed5ad79f6ecf8c19b8b04a57984a13c13b0768ac
sangwon432 <swonl0622@gmail.com>
2024-01-20 08:48:02 -0600

    login and token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LoggedinUserDto } from './dto/loggedin-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,12 @@ export class AuthController {
   @Post('/signup')
   async createUser(@Body() createUserDto: CreateUserDto) {
     return await this.authService.createUser(createUserDto);
+  }
+
+  @Post('/login')
+  async loggedinUser(@Body() loggedinUserDto: LoggedinUserDto) {
+    const user = await this.authService.logInUser(loggedinUserDto);
+    const token = await this.authService.generateAccessToken(user.id);
+    return { user, token };
   }
 }

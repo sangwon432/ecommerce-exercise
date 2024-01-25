b0a0943dc7eaec9516a18091b3f058fa0a41a13b
sangwon432 <swonl0622@gmail.com>
2024-01-25 15:24:34 -0600

    added token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,7 +18,10 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   async loggedinUser(@Req() req: RequestWithUserInterface) {
-    return await req.user;
+    // return await req.user;
+    const { user } = req;
+    const token = await this.authService.generateAccessToken(user.id);
+    return { user, token };
   }
   // async loggedinUser(@Body() loggedinUserDto: LoggedinUserDto) {
   //   const user = await this.authService.logInUser(loggedinUserDto);
